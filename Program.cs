1a882936b4090dacd6c84299845a8900555c20de
David L. Almeida <55282271+DavidAlmeidaDesigner@users.noreply.github.com>
2021-11-08 12:00:26 -0300

    Iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAD.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Criando um App simples de cadastro de séries em .NET
+
+## Professor: Eliézer Zarpelão
+Github: [github.com/elizarp](https://github.com/elizarp)
